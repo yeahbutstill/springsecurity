9736138523a0d4b860a71baafd95d89c7b56446f
yeahbutstill <mayadani28@gmail.com>
2022-10-24 12:12:08 +0700

    add config update error message

---
+++ src/main/java/com/yeahbutstill/springsecurity/demo/config/DemoSecurityConfig.java
@@ -0,0 +1,38 @@
+package com.yeahbutstill.springsecurity.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class DemoSecurityConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("PELAKSANA")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                        .anyRequest().authenticated()
+                        .and()
+                        .formLogin()
+                            .loginPage("/showMyLoginPage")
+                            .loginProcessingUrl("/authenticateTheUser")
+                            .permitAll();
+        return http.build();
+    }
+
+}
---
+++ src/main/java/com/yeahbutstill/springsecurity/demo/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,6 @@
+package com.yeahbutstill.springsecurity.demo.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+}
